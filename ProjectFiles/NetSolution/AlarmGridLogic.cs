61fb389e22ab9d75906a0b963a1c809dcbf31d19
Steven Liu <Deric.xu@automation.com>
2022-10-20 07:35:46 +0000

    aaa

---
+++ ProjectFiles/NetSolution/AlarmGridLogic.cs
@@ -0,0 +1,26 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.Retentivity;
+using FTOptix.NativeUI;
+using FTOptix.UI;
+using FTOptix.HMIProject;
+using FTOptix.Core;
+using FTOptix.CoreBase;
+using FTOptix.NetLogic;
+using FTOptix.Alarm;
+#endregion
+
+public class AlarmGridLogic : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+}
